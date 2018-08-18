10974615bb271d7f0c976e47c4ca8dd2c53f466e
Touhid Mia <hmtm.cse@gmail.com>
2018-08-18 16:32:14 +0600

    Integrating React with Grails

---
+++ react-app/webpack.config.js
@@ -0,0 +1,21 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '../grails-app/assets/javascripts/react'),
+        filename: 'bundle-[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
+
