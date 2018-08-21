73f066d2c0a7996ce427ddba210dba5540ae6326
Touhid Mia <hmtm.cse@gmail.com>
2018-08-21 21:57:13 +0600

    Grails Plugin

--- grails-app/init/com/hmtmcse/tm/BootStrap.groovy
+++ grails-app/init/com/hmtmcse/tm/BootStrap.groovy
@@ -3,7 +3,7 @@ package com.hmtmcse.tm
 class BootStrap {
 
     def init = { servletContext ->
-
+        println("Called Core Bootstrap")
         [1,2,3,4,5,6,7,8,9,10].each {
             print(it)
             new User(name: "Name " + it, email: "email " + it, password: "xxx " + it, uuid: "uuid " + it).save(flush:true)
