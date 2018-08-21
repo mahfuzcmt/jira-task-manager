536ac4b77a3112e87a2d70ebe3b33877ae8f2d06
Touhid Mia <hmtm.cse@gmail.com>
2018-08-21 18:18:18 +0600

    Integrate Material UI

--- grails-app/controllers/com/hmtmcse/tm/controller/DashboardController.groovy
+++ grails-app/controllers/com/hmtmcse/tm/controller/DashboardController.groovy
@@ -1,6 +1,11 @@
 package com.hmtmcse.tm.controller
 
+import com.hmtmcse.tm.User
+import grails.converters.JSON
+
 class DashboardController {
 
-    def index() { }
+    def index() {
+        render(User.list() as JSON)
+    }
 }
