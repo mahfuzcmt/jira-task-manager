c2f2ca85fd19a5e1c4dceb0fa5e7001f9ab77a83
Touhid Mia <touhid@bitmascot.com>
2018-08-31 12:34:35 +0600

    Adding Grails Swagger Plugin

--- grails-app/controllers/com/hmtmcse/tm/controller/api/ApiUserV2Controller.groovy
+++ grails-app/controllers/com/hmtmcse/tm/controller/api/ApiUserV2Controller.groovy
@@ -2,10 +2,22 @@ package com.hmtmcse.tm.controller.api
 
 import com.hmtmcse.tm.User
 import grails.converters.JSON
+import grails.rest.RestfulController
 
-class ApiUserV2Controller {
+class ApiUserV2Controller{
+
 
     def index() {
+        render(User.list() as JSON)
+        println("Yes")
+    }
+
+    def xyz() {
+        JSON.registerObjectMarshaller(User){
+            def output = [:]
+            output['id'] = it.id
+            return output
+        }
         render(User.list() as JSON)
     }
 }
